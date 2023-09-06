b936de85e6baa367e7c39ca76741cd8986723a26
Muhammad fasil <fazilfaztml@gmail.com>
2023-09-06 12:04:49 +0530

    first commit

---
+++ weather_application/lib/core/core.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle text = GoogleFonts.poppins(
+    textStyle: const TextStyle(color: Colors.white, fontSize: 30));
---
+++ weather_application/lib/home_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: size.height * 0.75,
+              width: size.width,
+              padding: const EdgeInsets.only(top: 30),
+              margin: const EdgeInsets.only(right: 10, left: 10),
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  gradient: const LinearGradient(
+                      colors: [Color(0xff955cd1), Color(0xff3fa2fa)],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      stops: [0.2, 0.85]),
+                  borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                children: [
+                  Text(
+                    'Constantine',
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.9),
+                        fontSize: 35,
+                        fontFamily: 'MavenPro'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Monday,05 Mars',
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.9),
+                        fontSize: 15,
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'MavenPro'),
+                  ),
+                  Image.asset(
+                    'assets/image/icons8-sunny-100.png',
+                    width: size.width * 0.4,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                 const  Text(
+                    'Sunny',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Hubbali'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    '15',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 75,
+                        fontWeight: FontWeight.w800,
+                        fontFamily: 'Hubbali'),
+                  ),
+                  Row(children: [Expanded(child: Column(children: [
+                    Image.asset('assets/image/icons8-windwhite-100.png',width: size.width * 0.15,),
+                    const  Text('17.1 km/h',style:  TextStyle(color: Colors.white,fontFamily: 'Hubballi',fontSize: 20),)
+
+                  ],))],)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
